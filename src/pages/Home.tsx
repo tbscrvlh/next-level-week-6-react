272bc1ce6c4075266586f66c62a0c3a5352034c1
Tobias Carvalho <tobias.jung.carvalho@gmail.com>
2021-06-23 14:30:28 -0300

    aula 3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,50 +1,79 @@
-import { useHistory } from 'react-router-dom';
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
-import '../styles/auth.scss';
-import { Button } from '../components/Button';
-import { useAuth } from '../hooks/useAuth';
+import { useHistory } from "react-router-dom";
+import { FormEvent, useState } from "react";
+
+import illustationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImage from "../assets/images/google-icon.svg";
+
+import { database } from "../services/firebase";
+
+import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
+
+import "../styles/auth.scss";
 
 export function Home() {
-    const history = useHistory();
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState("");
 
-    const { user, signInWithGoogle } = useAuth();
-
-    async function handleCreateRoom() {
-        if (!user) {
-            await signInWithGoogle()
-        }
-        history.push('/rooms/new');
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
     }
 
+    history.push("/rooms/new");
+  }
 
-    return (
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo real</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoImg} alt="Logo do letmeask" />
-                    <button onClick={handleCreateRoom} className="create-room">
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">ou entre em uma sala</div>
-                    <form>
-                        <input
-                            type="text"
-                            placeholder="Digite o código da sala"
-                        />
-                        <Button type="submit">
-                            Entrar na sala
-                        </Button>
-                    </form>
-                </div>
-            </main>
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === "") {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exists.");
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salar de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="LetMeAsk" />
+          <button className="create-room" onClick={handleCreateRoom}>
+            <img src={googleIconImage} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+
+          <div className="separator">ou entre em uma sala</div>
+
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
+
+            <Button type="submit">Entrar na sala</Button>
+          </form>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
